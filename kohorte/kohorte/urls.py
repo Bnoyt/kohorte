1d541df688f764bb55a6b81b4e65d391a39eb460
cartoonnerie <melodia@melix.org>
2017-12-13 16:34:09 +0100

    correction url 2 markdownx

--- kohorte/kohorte/urls.py
+++ kohorte/kohorte/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from markdownx import urls as markdownx
+from markdownx import urls
 
 urlpatterns = [
     url(r'^', include('app.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^markdownx/', include(markdownx)),
+    url(r'^markdownx/', include(markdownx.urls)),
 ]
