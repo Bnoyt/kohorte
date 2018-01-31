250b3a742045a7a421d4a67b17ecc895782025a2
Riernar <riernar@users.noreply.github.com>
2018-01-31 17:06:44 +0100

    django ready for startup code

--- app/apps.py
+++ app/apps.py
@@ -3,7 +3,3 @@ from django.apps import AppConfig
 
 class AppConfig(AppConfig):
     name = 'app'
-
-    def ready():
-        #TODO: Add startup code here
-        pass
---
+++ kohorte/startup.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+def run():
+    #TODO: put startup code here
+    print('Startup code in startup.py')
