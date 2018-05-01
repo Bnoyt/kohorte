87cb7266cf204f60f7fa8802046d1115cb24203a
Riernar <riernar@users.noreply.github.com>
2018-05-01 11:22:03 +0200

    adding command for projet initialisation

---
+++ app/management/commands/project.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  1 11:18:41 2018
+"""
+
---
+++ app/management/commands/project_subcommands/create.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from django.core.management.base import BaseCommand
+from app.backend.network import MessageHandler
+
+# Reference :
+# https://docs.djangoproject.com/en/dev/howto/custom-management-commands/#howto-custom-management-commands
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('question', help='The question for this project')
+
+    def handle(self, *args, **options):
+        question = options['question']
+        
+        
+        
+        msg = {'type': 'command',
+               'method_name': 'init_project',
+               'args': (question,)}
+        MessageHandler.send_python(msg)
