5feb4458b01b2a41a2f97203aacc0705c4755e34
Riernar <riernar@users.noreply.github.com>
2018-05-09 17:22:02 +0200

    progress in create command

--- app/management/commands/project_subcommands/create.py
+++ app/management/commands/project_subcommands/create.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
+import pathlib
 
 from django.core.management.base import BaseCommand
-from app.backend.network import MessageHandler
-
-import app.models as models
 from django.contrib.auth.models import User
+
+from app.backend.network import MessageHandler
+from app.clustering.parameters import memory_path
+import app.models as models
 
 # Reference :
 # https://docs.djangoproject.com/en/dev/howto/custom-management-commands/#howto-custom-management-commands
@@ -20,21 +22,22 @@ class Command(BaseCommand):
         try:
             question = options['question']
             authorname = options['authorname']
-            #FileSytem Initialisation
-            
             
             #Database Initialisation
-            if authorname:
-                usermatch = User.objects.all().filter(username=authorname)
-                if len(usermatch) == 1:
-                    new_project = models.Question(label=question, auteur=usermatch[0])
-                    new_project.save()
-                    self.stdout.write("Created new project with id " + str(new_project.id) + "\n")
-                else:
-                    self.stderr.write("Unknown username for author or multiple users\n")
+            usermatch = User.objects.all().filter(username=authorname)
+            if len(usermatch) == 1:
+                new_project = models.Question(label=question, auteur=usermatch[0])
+                new_project.save()
+                self.stdout.write("Created new project with id " + str(new_project.id) + "\n")
+            else:
+                raise ValueError("Unknown username for author or multiple users\n")
+            #FileSystem Initialisation
+            root = 
+            
         except Exception as e:
-            pass
+            self.stderr.write(e)
         else:
+            #Starting Backend thread for the new project
             msg = {'type': 'command', 'method_name': '_init_project', 'args': (new_project.id,)}
             MessageHandler.send_python(msg)
         
